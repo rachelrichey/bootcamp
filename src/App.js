7b42f62c920270dec924ad3c39ebbad7fdbdadc9
Rachel <rachelannerichey@gmail.com>
2024-01-20 14:13:17 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import CardEditor from './CardEditor';
 import CardViewer from './CardViewer';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './HomePage';
+import './styles.css';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Homepage />
-      </Route>
-      <Route exact path="/editor">
-        <CardEditor />
-      </Route>
-      <Route exact path="/viewer/:deckId">
-        <CardViewer />
-      </Route>
-      <Route>
-        <div>Page not found!</div>
-      </Route>
-    </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route exact path="/editor">
+          <CardEditor />
+        </Route>
+        <Route exact path="/viewer/:deckId">
+          <CardViewer />
+        </Route>
+        <Route>
+          <div>Page not found!</div>
+        </Route>
+      </Switch>
   );
 };
 
